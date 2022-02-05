addd5ebcc9f2baa9d67909475e640f38c64537a5
Devi1k <570541309@qq.com>
2022-02-05 17:20:40 +0800

    add log

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # GovSpeechRecognition
+
+## Quick Start
+
+```shell
+cd root/GovSpeechRecognition
+# source the environment
+conda activate speech
+# run program
+make run
+```
--- uploader/views.py
+++ uploader/views.py
@@ -1,35 +1,40 @@
-import array
-import json
 import os
 import random
+import re
 import struct
 import time
 import wave
+import logging
+import datetime
+from logging.handlers import TimedRotatingFileHandler
 from time import gmtime
 from time import strftime
 
-import numpy
-import scipy
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django import forms
-import functools
 
 import numpy as np
 import paddle
 
-from scipy.io import wavfile
 from paddle.io import DataLoader
 
 from paddlespeech.s2t.exps.deepspeech2.config import get_cfg_defaults
 from paddlespeech.s2t.io.collator import SpeechCollator
 from paddlespeech.s2t.io.dataset import ManifestDataset
 from paddlespeech.s2t.models.ds2 import DeepSpeech2Model
-from paddlespeech.s2t.training.cli import default_argument_parser
-from paddlespeech.s2t.utils.utility import add_arguments
-from paddlespeech.s2t.utils.utility import print_arguments
 from paddlespeech.s2t.frontend.utility import read_manifest
+
+LOG_PATH = '../log'
+log_fmt = '%(asctime)s\tFile \"%(filename)s\",line %(lineno)s\t%(levelname)s: %(message)s'
+formatter = logging.Formatter(log_fmt)
+log_file_handler = TimedRotatingFileHandler(filename=LOG_PATH, when="D", interval=1, backupCount=7)
+log_file_handler.suffix = "%Y-%m-%d_%H-%M.log"
+log_file_handler.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}_\d{2}-\d{2}.log$")
+log_file_handler.setFormatter(formatter)
+log_file_handler.setLevel(logging.DEBUG)
+log = logging.getLogger()
+log.addHandler(log_file_handler)
 
 speech_save_dir = 'data/demo_cache'  # Directory to save demo audios.
 warmup_manifest = 'data/manifest.dev'  # Filepath of manifest to warm up.
@@ -57,7 +62,7 @@ def file_to_transcript(filename):
     feature = test_loader.collate_fn.process_utterance(filename, "")
     audio = np.array([feature[0]]).astype('float32')  # [1, T, D]
     # audio = audio.swapaxes(1,2)
-    print('---file_to_transcript feature----')
+    log.debug('---file_to_transcript feature----')
     # print(audio.shape)
     audio_len = feature[0].shape[0]
     # print(audio_len)
@@ -88,16 +93,16 @@ def warm_up_test(audio_process_handler,
     rng = random.Random(random_seed)
     samples = rng.sample(manifest, num_test_cases)
     for idx, sample in enumerate(samples):
-        print("Warm-up Test Case %d: %s" % (idx, sample['feat']))
+        log.debug("Warm-up Test Case %d: %s" % (idx, sample['feat']))
         start_time = time.time()
         transcript = audio_process_handler(sample['feat'])
         finish_time = time.time()
-        print("Response Time: %f, Transcript: %s" %
-              (finish_time - start_time, transcript))
+        log.debug("Response Time: %f, Transcript: %s" %
+                  (finish_time - start_time, transcript))
 
 
 # # warming up with utterrances sampled from Librispeech
-print('Warming up ...')
+log.debug('Warming up ...')
 warm_up_test(
     audio_process_handler=file_to_transcript,
     manifest_path=warmup_manifest,
@@ -109,27 +114,41 @@ def handle_uploaded_file(file, model_config, speech_save_dir, test_loader, check
     # model.eval()
     if not os.path.exists(speech_save_dir):
         os.mkdir(speech_save_dir)
-    # timestamp = strftime("%Y%m%d%H%M%S", gmtime())
-    # out_filename = os.path.join(
-    #     speech_save_dir,
-    #     timestamp + "_" + ".wav")
-    # write to wav file
-    # print('write successfully')
-    # with open(out_filename, 'wb+') as destination:
-    #     for chunk in file.chunks():
-    #         destination.write(chunk)
+
     start_time = time.time()
     transcript = audio_process_handler(file)
     finish_time = time.time()
-    print("Response Time: %f, Transcript: %s" %
-          (finish_time - start_time, transcript))
+    log.debug("Response Time: %f, Transcript: %s" %
+              (finish_time - start_time, transcript))
     return transcript.encode('utf-8')
+
+
+def clean_cache():
+    path = '../data/demo_cache/'
+    for i in os.listdir(path):
+        file_path = path + i  # 生成日志文件的路径
+        timestamp = strftime("%Y%m%d%H%M%S", gmtime())
+        # 获取日志的年月，和今天的年月
+        today_m = int(timestamp[4:5])  # 今天的月份
+        m = int(i[4:5])  # 日志的月份
+        today_y = int(timestamp[0:4])  # 今天的年份
+        y = int(i[0:4])  # 日志的年份
+
+        # 对上个月的日志进行清理，即删除。
+        if m < today_m:
+            if os.path.exists(file_path):  # 判断生成的路径对不对，防止报错
+                os.remove(file_path)  # 删除文件
+        elif y < today_y:
+            if os.path.exists(file_path):
+                os.remove(file_path)
 
 
 @csrf_exempt
 def upload_file(request):
-    print('-----------------------------------------------------------')
-    print('-----------------------------------------------------------')
+    clean_cache()
+    log.debug('-----------------------------------------------------------')
+    log.debug('-----------------------------------------------------------')
+
     body_content = request.body
     # print(body_content)
 
@@ -149,7 +168,6 @@ def upload_file(request):
         # audio.writeframes(blob.read())
         # audio.close()
 
-        print('-------------body-----------------')
         # print(body_content[:138])
         with open(out_filename, 'wb+') as destination:
             destination.write(body_content[44:])
@@ -159,8 +177,6 @@ def upload_file(request):
         # print(wav_file[0:45])
 
         # print(struct.unpack('h', wav_file[0:2])[0])
-        print(struct.unpack('h', wav_file[22:24])[0])
-        print(struct.unpack('<i', wav_file[24:28])[0])
         # print(struct.unpack('<i', wav_file[40:44])[0])
         file = wave.open(out_filename, 'wb')
         file.setnchannels(2)
@@ -168,21 +184,18 @@ def upload_file(request):
         file.setframerate(48000)
         file.writeframes(wav_file)
         file.close()
-        print('write successfully')
 
-        print("Saved to %s." % out_filename)
+        log.debug("Saved to %s." % out_filename)
 
-        print('----------------file-----------------------')
         # print('file ' + f)
         if out_filename:
-            print('--------------success get file---------------')
             start_time = time.time()
             context = handle_uploaded_file(file=out_filename, model_config=config, speech_save_dir=speech_save_dir,
                                            test_loader=test_loader,
                                            checkpoint_path=checkpoint_path,
                                            audio_process_handler=file_to_transcript)
             end_time = time.time()
-            print(end_time - start_time)
+            # log.debug(end_time - start_time)
             context = str(context, encoding="utf-8")
             return JsonResponse({'message': 'success', 'data': context, 'code': 0})
         return JsonResponse({'message': 'unknown file', 'code': 50013})
